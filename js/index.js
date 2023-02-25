7b0cf83a11a0af4bd51cbac2fe7638f7c963b6cc
Muhammad Maarig <81269436+mmaarig@users.noreply.github.com>
2023-02-25 16:54:20 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,14 +14,6 @@ function myFunction() {
     document.getElementById("demo2").innerHTML = "Your Average Blood Sugar in mg/dl is probably: "+mgdl;
     }
 
-// function myFunction3() {
-//    var basic = prompt("Please Enter Your Basic Salary ") //user input for Hourly Overtime result
-//    var ot = ((basic/26)/8)*(Number(2))
-//    ot = ot.toFixed(0) //limiting decimals to 2 digits
-//    //document.write("Your Overtime is probably: ",dcct,"%<br><br>") //printing the Number
-//    document.getElementById("demo3").innerHTML = "Your Overtime is probably: "+ot;
-//    }
-
    function myFunction3() {
     var basic = prompt("Please Enter Your Basic Salary ") //user input for Hourly Overtime result
     var hours = prompt("Please Enter Your Hours ") //user input for Hourly Overtime result
